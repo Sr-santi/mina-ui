f4797460d21fe348290f6f17103dd339749a7eed
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2022-10-07 23:20:03 -0500

    Consuming the smart contract using npm link for fast testing is done

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,13 +1,22 @@
 import ActionButton from '@Components/ActionButton';
-import React, { ReactNode } from 'react';
+import React, { ReactNode,useState,useEffect } from 'react';
 import { HeaderContainer, HeaderLogo, HeaderLogoContainer, HeaderWrapper } from './styles';
 import { SudokuZkApp } from 'sudoku'; //Linking the smart contract to consume it in the UI 
+
 
 interface Props {
   children?: ReactNode;
 }
+useEffect(() => {
+  (async () => {
+    const { SudokuZkApp } = await import("sudoku");
+  })();
+}, []);
 
 export default function Header({ children }: Props) {
+  // const handleClick = () => {
+  //   console.log('Click happened');
+  // };
   return (
     <React.Fragment>
         <HeaderContainer className='px-12'>
