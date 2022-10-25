1cf26a9740cb86fd56664d1608cc5737e0f8b815
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2022-10-24 20:49:34 -0500

    More structure to the smart contract

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -56,7 +56,13 @@ class MixerZkapp extends SmartContract {
 // setup
 const Local = Mina.LocalBlockchain();
 Mina.setActiveInstance(Local);
-let feePayer = Local.testAccounts[0].privateKey;
+// a test account that pays all the fees, and puts additional funds into the zkapp
+//For our Mixer case the HarpoFeePayer will be the HarpoAccount
+let minadoFeePayer = Local.testAccounts[0].privateKey;
+let minadoFeePayerAccount = minadoFeePayer.toPublicKey();
+
+//This initial balance will fund our harpoFeePayer
+let initialBalance = 10_000_000_000;
 
 //TODO: ADD STATE INTERFACE
 type Interface = {
@@ -67,7 +73,7 @@ let isDeploying = null as null | Interface;
 
 async function deploy() {
   if (isDeploying) return isDeploying;
-
+// ZK APP ACCOUNT 
   let zkappKey = PrivateKey.random();
   let zkappAddress = zkappKey.toPublicKey();
   tic('compile');
@@ -82,15 +88,91 @@ async function deploy() {
   // };
 
   let zkapp = new MixerZkapp(zkappAddress);
-  let tx = await Mina.transaction(feePayer, () => {
-    AccountUpdate.fundNewAccount(feePayer);
+  let tx = await Mina.transaction(minadoFeePayer, () => {
+    AccountUpdate.fundNewAccount(minadoFeePayer,{initialBalance});
     zkapp.deploy({ zkappKey,verificationKey });
+    console.log('Minado wallet funded succesfully');
   });
   await tx.send().wait();
 
   isDeploying = null;
   // return zkappInterface;
 }
+
+//TODO ADD INTEGRATION WITH ARURO WALLET
+
+// Creating a user account that wants to use Harpo
+//TODO Replace with real address coming from AurO;
+let userAccountKey = PrivateKey.random();
+let userAccountAddress = userAccountKey.toPublicKey();
+
+
+/**
+ * Deposit  Logic
+ * 1. A Minado  account that will pay the gas feeds is funded DONE IN Deploy function
+ * 2. A userAccount is  funded with the purpose of depositing into our harpoAccount.
+ * Note: In a real implementation this would not happen as the account already has a balance
+ * 3. A commitment needs to be created  C(0) = H(S(0),N(0))
+ * 3.1 A Secret is created using Poseidon
+ * 3.2 A Nullifier is created for avoiding double spending
+ * 3.3 The Secret and the Nullifier is hashed and the commitment is created
+ * 4. The Merkle path root must be verified.
+ * 5. Add commitment to the Merkle Tree
+ * 6. Send funds from useraccount to MerkleTree
+ */
+/**
+ * 2. A userAccount is  funded with the purpose of depositing into our harpoAccount.
+ * Note: Will not happen in a real implementation
+ * TODO: Replace with Auro wallet Logic
+ */
+
+async function depositTestFunds () {
+  let tx2 = await Mina.transaction(minadoFeePayer, () => {
+    AccountUpdate.fundNewAccount(minadoFeePayer);
+    let update = AccountUpdate.createSigned(minadoFeePayer);
+    //The userAddress is funced
+    update.send({ to: userAccountAddress, amount: 20 });
+    console.log('User account wallet funded');
+  });
+  console.log('Second TX');
+  await tx2.send();
+  console.log('UserWallet funded succesfully');
+}
+
+function verifyAccountBalance (address:any) {
+  let balance= Mina.getBalance(userAccountAddress);
+  console.log(`Balance from ${address} = ${balance} MINA`)
+  return balance; 
+}
+/**
+ * 3. A commitment needs to be created  C(0) = H(S(0),N(0))
+ */
+ let nullifier = await createNullifier(userAccountAddress);
+ let commitment = await createCommitment(nullifier);
+
+ /**
+ * Function to create Nullifier Nullifier: H ( Spending Key, rho )
+ * Spending key: Public key
+ * Rho: Private key
+ */
+
+async function createNullifier(publicKey: PublicKey) {
+  let keyString = publicKey.toFields();
+  let secretField = Field.random();
+  let nullifierHash = Poseidon.hash([...keyString, secretField]);
+
+  return nullifierHash;
+}
+
+/**
+ * Function to create  the Commitment C(0) = H(S(0),N(0))
+ */
+ async function createCommitment(nullifier: any) {
+  let secret = Field.random();
+  let commitment = Poseidon.hash([nullifier, secret]);
+  return commitment;
+}
+
 
 //TODO ADD STATE VARIABLES 
 // function getState(zkappAddress: PublicKey) {
